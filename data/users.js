54ab56985764be3d3b4336b249b73ce045b28d60
Israajddn <Israajddn@gmail.com>
2023-11-12 10:56:05 -0500

    start building

---
+++ data/users.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+      id: 1,
+      name: "John",
+      username: "John23",
+      email: "John23@example.com",
+    },
+    {
+      id: 2,
+      name: "Jan",
+      username: "Jan",
+      email: "Jan_u@example.com",
+    },
+    {
+      id: 3,
+      name: "Jim",
+      username: "Jim",
+      email: "Jim@example.com",
+    },
+  ];
+  
+  module.exports = users;
+  
